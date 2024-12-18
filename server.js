f551ee6af3a3993462e05f36f539ac1f91e14406
ishubham-nagarro <shubham.bansal02@nagarro.com>
2024-12-18 13:53:34 +0530

    Remove http module

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { authRoutes } from './src/routes/authRoutes.js';
 import { userRoutes } from './src/routes/userRoutes.js';
 
 const app = express();
-const server = http.createServer(app)
 dotenv.config()
 
 // Middleware
@@ -28,4 +26,4 @@ mongoose.connect(process.env.MONGO_URI, {
     .catch(err => console.error('MongoDB connection error:', err));
   
   const PORT = process.env.PORT || 5000;
-  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
